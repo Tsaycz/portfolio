b217dec34fddf45f0f38c0377898e11e54167f44
Tsaycz <tgkjouka@gmail.com>
2022-10-05 16:00:56 -0500

    test

---
+++ Gomoku/src/main/java/learn/App.java
@@ -0,0 +1,8 @@
+package learn;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("If you're reading this, you've found my portfolio. Everything is being moved to this Repo.");
+    }
+}
